3ff7a5c67f2831119ac14ba87c592b414956f795
pdroa3165-droid <pdroa3165@gmail.com>
2026-03-26 20:40:08 -0300

    Add files via upload

---
+++ aula2603.py
@@ -0,0 +1,21 @@
+cliente = input("Qual seu CPF?")
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+print("Banco Exxemplo")
+print("Usuário: Bia")
+print("CPF:",cliente)
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+print("Selecione uma opção: 1- Saldo / 2- Extratos (Últimos 5 dias) / 3- Emprétimos")
+opção = int(input())
+if opção == 1:
+    print("Saldo:00,01")
+elif opção == 2:
+    print("Extratos: Você não fez nenhuma movimentação nos útimos dias.")
+elif opção == 3:
+    print("Empréstimos: Não são possíveis no momento.")
+elif opção <=0:
+    print("Opção inválida")
+elif opção >3:
+    print("Opção inválida")
+
+
+
