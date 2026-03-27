7af9f810ebadac0ca70f01b79c65c0cffe84a260
pdroa3165-droid <pdroa3165@gmail.com>
2026-03-26 21:19:28 -0300

    Update aula2603.py

--- aula2603.py
+++ aula2603.py
@@ -1,22 +1,24 @@
 # Primeiro menu em Python
-cliente = input("Qual seu CPF?")
+print("Qual seu CPF? Sem pontos ou traços")
+CPF = int(input())
 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 print("Banco Exxemplo")
 print("Usuário: Bia")
-print("CPF:",cliente)
+print("CPF:",CPF)
 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-print("Selecione uma opção: 1- Saldo / 2- Extratos (Últimos 5 dias) / 3- Emprétimos")
+print("~Selecione uma opção:") 
+print("1- Saldo") 
+print("2- Extratos (Últimos 5 dias)")  
+print("3- Emprétimos")
 opção = int(input())
 if opção == 1:
-    print("Saldo:00,01")
+    print("~Saldo:00,01")
 elif opção == 2:
-    print("Extratos: Você não fez nenhuma movimentação nos útimos dias.")
+    print("~Extratos: Você não fez nenhuma movimentação nos útimos dias.")
 elif opção == 3:
-    print("Empréstimos: Não são possíveis no momento.")
+    print("~Empréstimos: Não são possíveis no momento.")
 elif opção <=0:
-    print("Opção inválida")
+    print("~Opção inválida")
 elif opção >3:
-    print("Opção inválida")
-
-
+    print("~Opção inválida")
 
